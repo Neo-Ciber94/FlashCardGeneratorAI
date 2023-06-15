e1c134f3c30fa46d2255d1a3567a009514d77152
Freddy L <neo-ciber_94@hotmail.com>
2023-06-15 02:50:56 -0400

    Updated auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "flashcardgeneratorai5c19902d": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "flashcarddev": {
       "Arn": "string",
--- src/pages/auth/[...auth].tsx
+++ src/pages/auth/[...auth].tsx
@@ -2,8 +2,12 @@ import Redirect from "@/lib/components/Redirect";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
 export default function AuthPage() {
-  const { user } = useAuthenticator((ctx) => [ctx.user]);
-  console.log({ user });
+  const { user, isPending, authStatus } = useAuthenticator((ctx) => [ctx.user]);
+  console.log({ user, isPending, authStatus });
+
+  if (authStatus === "configuring") {
+    return "Loading...";
+  }
 
   return <Redirect to="/" />;
 }

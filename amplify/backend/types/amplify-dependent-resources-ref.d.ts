58152a71acd41bda2fe8ff336249df8a7cc50e07
Freddy L <neo-ciber_94@hotmail.com>
2023-06-17 01:18:53 -0400

    Updated auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,17 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "flashcardgeneratorai0f3170a7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "storage": {
     "flashcarddev": {
       "Arn": "string",

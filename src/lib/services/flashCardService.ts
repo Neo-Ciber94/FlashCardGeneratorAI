7e5c7fcdb1652b3cf57a89f9432f85dbe24feda5
Freddy L <neo-ciber_94@hotmail.com>
2023-06-18 17:12:44 -0400

    minor changes

--- src/lib/services/flashCardService.ts
+++ src/lib/services/flashCardService.ts
@@ -178,7 +178,7 @@ export class FlashcardService {
                 content: card.content,
                 topicId: input.topicId,
                 ownerId: userId,
-                lastModified: now + Math.floor(Math.random() * 1000),
+                lastModified: now,
                 isAiGenerated: true
             }
         });

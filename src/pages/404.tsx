30c97f32b2018bc073ac7dad78a2e3e9c070715e
Freddy L <neo-ciber_94@hotmail.com>
2023-06-18 17:48:29 -0400

    Added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <h1 className="flex h-[60vh] w-full flex-row items-center justify-center text-4xl text-red-500/50">
+      Not Found
+    </h1>
+  );
+}

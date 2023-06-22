3ec89b0cae4932e542e19a79d3df21bb29276724
Freddy L <neo-ciber_94@hotmail.com>
2023-06-22 19:08:20 -0400

    feat: Added custom error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,5 @@
+import ErrorPage from "./_error";
+
 export default function NotFound() {
-  return (
-    <h1 className="flex h-[60vh] w-full flex-row items-center justify-center text-4xl text-red-500/50">
-      Not Found
-    </h1>
-  );
+  return <ErrorPage statusCode={404} />;
 }
---
+++ src/pages/_error.tsx
@@ -0,0 +1,18 @@
+import { STATUS_CODES } from "http";
+
+export interface ErrorPageProps {
+  statusCode: number;
+  message?: string;
+}
+
+export default function ErrorPage({ statusCode, message }: ErrorPageProps) {
+  return (
+    <h1 className="flex h-[60vh] w-full flex-row items-center justify-center text-2xl sm:text-4xl gap-2">
+      <span className="text-red-500/50">{statusCode}</span>
+      <span className="text-red-500/50">|</span>
+      <span className="text-red-500/50">
+        {STATUS_CODES[statusCode] || message}
+      </span>
+    </h1>
+  );
+}

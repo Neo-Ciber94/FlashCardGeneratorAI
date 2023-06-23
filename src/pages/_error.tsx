1faeecca3f7e802184656cef590036279ca75fe1
Freddy L <neo-ciber_94@hotmail.com>
2023-06-22 20:51:02 -0400

    Updated error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,13 +1,14 @@
 import { STATUS_CODES } from "http";
+import { NextApiRequest } from "next";
 
 export interface ErrorPageProps {
   statusCode: number;
   message?: string;
 }
 
-export default function ErrorPage({ statusCode, message }: ErrorPageProps) {
+export default function ErrorPage({ statusCode = 500, message }: ErrorPageProps) {
   return (
-    <h1 className="flex h-[60vh] w-full flex-row items-center justify-center text-xl sm:text-2xl gap-2">
+    <h1 className="flex h-[60vh] w-full flex-row items-center justify-center gap-2 text-xl sm:text-2xl">
       <span className="text-red-500/50">{statusCode}</span>
       <span className="text-red-500/50">|</span>
       <span className="text-red-500/50">
@@ -16,3 +17,13 @@ export default function ErrorPage({ statusCode, message }: ErrorPageProps) {
     </h1>
   );
 }
+
+interface ErrorPageInitialProps {
+  res: NextApiRequest;
+  err: { statusCode: number };
+}
+
+ErrorPage.getInitialProps = ({ res, err }: ErrorPageInitialProps) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
